345661210713b0c6ae955f6728b839ec82c81978
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2021-04-21 16:29:25 -0500

    building ourMission first info section

--- src/components/OurMission/index.js
+++ src/components/OurMission/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Container } from './styles/ourMission';
+import { Container, InfoOne } from './styles/ourMission';
 
 export default function OurMission({children, ...restProps}) {
   return <Container {...restProps}>{children}</Container>
 }
 
-OurMission.InfoOne = function OutMissionInfoOne({...restProps, children}) {
+OurMission.InfoOne = function OutMissionInfoOne({children, ...restProps}) {
   return (
-    <InfoOne>
-      
+    <InfoOne {...restProps}>
+      <div className="left">
+        <h1>Image</h1>
+      </div>
+      <div className="right">
+        <h3>Title</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil sint consectetur in earum, incidunt nesciunt exercitationem! Sit, pariatur consequuntur quas quam cumque explicabo, nihil perferendis veritatis nulla odit tempora autem quisquam nesciunt beatae. Consequuntur nihil, ducimus expedita perspiciatis enim id!
+        </p>
+      </div>
     </InfoOne>
   )
 }
--- src/components/OurMission/styles/ourMission.js
+++ src/components/OurMission/styles/ourMission.js
@@ -4,3 +4,11 @@ export const Container = styled.section`
   border: 2px solid yellow;
   height: 100vh;
 `;
+export const InfoOne = styled.div`
+  width: 100%;
+  ${'' /* height: 100%; */}
+  display: grid;
+  grid-template-columns: repeat(1fr, 2);
+  border: 2px solid red;
+  background: #fff;
+`;
