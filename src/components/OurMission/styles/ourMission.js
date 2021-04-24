cc91f7ff30dd0805ed5218fbe28b405659572a7f
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2021-04-23 20:50:29 -0500

    added mobile responsive design to OurMission section

--- src/components/OurMission/styles/ourMission.js
+++ src/components/OurMission/styles/ourMission.js
@@ -4,7 +4,7 @@ export const Container = styled.section`
   ${'' /* border: 2px solid yellow; */}
   ${'' /* height: 100vh; */}
   ${'' /* padding: 0 50px; */}
-  width: 1000px;
+  max-width: 1000px;
   margin: 0 auto;
 `;
 
@@ -16,21 +16,24 @@ export const Info = styled.div`
   color: #fff;
   margin-bottom: 8em;
   gap: 1em;
-  .left, .right {
-    ${'' /* border: 2px solid orange; */}
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    ${'' /* width: 50%; */}
-  }
+  padding: 0 25px;
 
-  .left {
-    ${'' /* background: transparent; */}
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(2, 1fr);
+    margin-bottom: 4em;
+    gap: 0;
     img {
-      ${'' /* display: block; */}
+      ${'' /* width: 100%; */}
+      height: 100px;
+    }
+  }
+  .left {
+    img {
       width: 100%;
       height: auto;
     }
+    
   }
 
   .right {
@@ -41,11 +44,31 @@ export const Info = styled.div`
     letter-spacing: .2px;
     padding: 20px;
     h3 {
-      padding: 10px 0;
-    }
-
-    h3 {
+      padding-bottom: 10px;
       font-size: 2rem;
     }
   }
+
+  .left, .right {
+    ${'' /* border: 2px solid orange; */}
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    @media (max-width: 768px) {
+      width: 80%;
+      margin: 0 auto;
+      img {
+        width: 60%;
+      }
+    }
+    @media (max-width: 600px) {
+      width: 100%;
+      img {
+        width: 80%;
+      }
+    }
+  }
+
+  
 `;
