46d14070a9bb3182b1d6834ed6a3c8da46f93748
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2021-04-28 19:32:39 -0500

    working on footer links

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -6,7 +6,7 @@ export function FooterContainer() {
    <Footer>
       <Footer.Logo src="./img/icons/chocolate-bar-pieces-blue.png" alt="Chocolate Bar Logo" />
       <Footer.Section>
-      <h4>Our Story</h4>
+        <h4>Our Story</h4>
         <ul>
         <li><a href="#OurMission">Our Mission</a></li>
         <li><a href="#OurTimeline">Our Timeline</a></li>
@@ -15,14 +15,14 @@ export function FooterContainer() {
         </ul>
       </Footer.Section>
       <Footer.Section>
-      <h4>Join In</h4>
+        <h4>Join In</h4>
         <ul>
           <li><a href="#WhatYouCanDo">See What You Can Do</a></li>
           <li><a href="#BecomeFriends">Become Serious Friends</a></li>
         </ul>
       </Footer.Section>
       <Footer.Section>
-      <h4>ChocoShop</h4>
+        <h4>ChocoShop</h4>
         <ul>
           <li><a href="#BigBars">Big Bars</a></li>
           <li><a href="#SmallBars">Small Bars</a></li>
@@ -31,7 +31,7 @@ export function FooterContainer() {
         </ul>
       </Footer.Section>
       <Footer.Section>
-      <h4>Other Stuff..</h4>
+        <h4>Other Stuff..</h4>
         <ul>
           <li>Team Tony's</li>
           <li>Careers</li>
