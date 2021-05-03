8c7a38edf31d6dd49faab9cf58fe093a6212f83b
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2021-05-03 16:19:06 -0500

    filtering chocolate bars by sizes

--- src/components/chocoShop/index.js
+++ src/components/chocoShop/index.js
@@ -3,6 +3,128 @@ import { Container, SidebarContainer, SidebarNav, MainMenuContainer, MainMenu, M
 
 export const FeatureContext = createContext();
 
+export const bigBars = [
+  {
+    title: "Milk Chocolate 32%",
+    subTitle: "6.35oz, 1 bar",
+    price: "$5.95",
+    image: "./img/big-bars/milk-chocolate-bar-red.png",
+    alt: "Milk Chocolate Bar"
+  },
+  {
+    title: "Milk Hazelnut 32%",
+    subTitle: "6.35oz, 1 bar",
+    price: "$5.95",
+    image: "./img/big-bars/milk-chocolate-hazelnut-green.png",
+    alt: "Milk Chocolate Hazelnut Bar"
+  },
+  {
+    title: "Milk Caramel Sea Salt 32%",
+    subTitle: "6.35oz, 1 bar",
+    price: "$5.95",
+    image: "./img/big-bars/milk-chocolate-seasalt-caramel-orange.png",
+    alt: "Milk Caramel Sea Salt Bar"
+  },
+  {
+    title: "Milk Honey Almond Nougat 32%",
+    subTitle: "6.35oz, 1 bar",
+    price: "$5.95",
+    image: "./img/big-bars/chocolate-honey-almond-nougat-yellow.png",
+    alt: "Milk Honey Almond Nougat"
+  },
+  {
+    title: "Dark Almond Sea Salt 51%",
+    subTitle: "6.35oz, 1 bar",
+    price: "$5.95",
+    image: "./img/big-bars/chocolate-almond-seasalt-limegreen.png",
+    alt: "Dark Almond Sea Salt"
+  },
+  {
+    title: "Dark Pecan Coconut 51%",
+    subTitle: "6.35oz, 1 bar",
+    price: "$5.95",
+    image: "./img/big-bars/dark-chocolate-pecan-coconut-teal.png" ,
+    alt: "Dark Pecan Coconut"
+  },
+  {
+    title: "Dark Chocolate 70%",
+    subTitle: "6.35oz, 1 bar",
+    price: "$5.95",
+    image: "./img/big-bars/dark-chocolate-blue.png"  ,
+    alt: "Dark Chocolate"
+  },
+  {
+    title: "Dark Milk Pretzel Toffee 42%",
+    subTitle: "6.35oz, 1 bar",
+    price: "$5.95",
+    image: "./img/big-bars/dark-milk-chocolate-pretzel-toffee-purple.png"  ,
+    alt: "Dark Milk Pretzel Toffee"
+  },
+  {
+    title: "White Raspberry Popping Candy 28%",
+    subTitle: "6.35oz, 1 bar",
+    price: "$5.95",
+    image: "./img/big-bars/white-raspberry-popping-candy-pink.png"  ,
+    alt: "White Raspberry Popping Candy"
+  },
+       
+];
+
+export const smallBars = [
+   {
+    title: "Milk Chocolate 32%",
+    subTitle: "1.8oz, 1 bar",
+    price: "$2.39",
+    image: "./img/small-bars/milk-chocolate-small-red.png"  ,
+    alt: "Milk Chocolate"
+  },
+  {
+    title: "Milk Caramel Sea Salt 32%",
+    subTitle: "1.8oz, 1 bar",
+    price: "$2.39",
+    image: "./img/small-bars/milk-caramel-sea-salt-small-orange.png"  ,
+    alt: "Milk Caramel Sea Salt"
+  },
+  {
+    title: "Dark Chocolate 70%",
+    subTitle: "1.8oz, 1 bar",
+    price: "$2.39",
+    image:"./img/small-bars/dark-chocolate-small-blue.png",
+    alt: "Dark Chocolate"
+  },      
+]
+
+export const tinyTonys = [
+  {
+    title: "Milk Chocolate 32% Tiny Tony's",
+    subTitle: "100 pieces",
+    price: "$48.69",
+    image:"./img/tiny-tonys/milk-chocolate/milk-chocolate-mini-open-small.png",
+    alt: "Milk Chocolate"
+  },
+  {
+    title: "Milk Caramel Sea Salt 32% Tiny Tony's",
+    subTitle: "100 pieces",
+    price: "$48.69",
+    image:"./img/tiny-tonys/milk-chocolate-caramel-seasalt/milk-chocolate-caramel-open.png" ,
+    alt: "Milk Chocolate Caramel Sea Salt Tiny Tony's"
+  },
+  {
+    title: "Dark Chocolate 70% Tiny Tony's",
+    subTitle: "100 pieces",
+    price: "$48.69",
+    image:"./img/tiny-tonys/dark-chocolate/dark-chocolate-mini-open-large.png"  ,
+    alt: "Dark Chocolate Tiny Tony's"
+  },
+]
+
+const allChocolates = bigBars.concat(smallBars).concat(tinyTonys)
+
+const filterBigBars = allChocolates.filter((bars) => bars.price === "$5.95")
+const filterSmallBars = allChocolates.filter((bars) => bars.price === "$2.39")
+const filterTinyTonys = allChocolates.filter((bars) => bars.price === "$48.69")
+console.log(filterTinyTonys)
+
 export default function ChocoShop({children, ...restProps}) {
   const [showModal, setShowModal] = useState(false);
   const [itemFeature, setItemFeature] = useState({});
@@ -15,6 +137,8 @@ export default function ChocoShop({children, ...restProps}) {
   )
 };
 
+//? Left Side Menu ===========================================
+
 ChocoShop.SidebarContainer = function ChocoShopSidebarContainer({ children, ...restProps}) {
    
   return <SidebarContainer {...restProps}>{children}</SidebarContainer>;
@@ -24,14 +148,33 @@ ChocoShop.SidebarNav = function ChocoShopSidebarNav({ref, children, ...restProps
 
   const sidebarRef = useRef(null);
   useEffect(() => {
+    //? Menu moves on scroll
     window.addEventListener('scroll', function() {
       const elem = sidebarRef.current;
-      // console.log(elem)
       elem.classList.toggle("sticky", window.scrollY > 0);
     });
   }, []);
-  return <SidebarNav ref={sidebarRef} {...restProps}>{children}</SidebarNav>;
+
+  return (
+    <SidebarNav 
+      ref={sidebarRef} 
+      {...restProps}
+    >
+      {/* {children} */}
+      <ul>
+        <a href="#" onClick={(e) => {
+          e.preventDefault()
+          return allChocolates
+          }}><li>All Chocolate</li></a>
+        <a href="#" onClick={() => bigBars}><li>Big Bars</li></a>
+        <a href="/ChocoShop"><li>Small Bars</li></a>
+        <a href="/ChocoShop"><li>Tiny Tony's</li></a>
+      </ul>
+    </SidebarNav>
+  );
 }
+
+//? Right Side Menu ============================================
 
 ChocoShop.MainMenuContainer = function ChocoShopMainMenu({children, ...restProps}) {
   return <MainMenuContainer {...restProps}>{children}</MainMenuContainer>;
@@ -55,9 +198,9 @@ ChocoShop.MenuItem = function ChocoShopMenuItem({children, ...restProps}) {
   // }, []);
   return (
     <MenuItem 
-      onClick={() => {
-        setShowModal(true)
-      }} 
+      // onClick={() => {
+      //   setShowModal(true)
+      // }} 
       {...restProps}>{children}</MenuItem>
   );
 }
--- src/containers/chocoShop.js
+++ src/containers/chocoShop.js
@@ -1,121 +1,122 @@
 import React from 'react';
-import { ChocoShop } from '../components'
+import { ChocoShop } from '../components';
+import { bigBars, smallBars, tinyTonys } from '../components/chocoShop'
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 
-const bigBars = [
-  {
-    title: "Milk Chocolate 32%",
-    subTitle: "6.35oz, 1 bar",
-    price: "$5.95",
-    image: "./img/big-bars/milk-chocolate-bar-red.png",
-    alt: "Milk Chocolate Bar"
-  },
-  {
-    title: "Milk Hazelnut 32%",
-    subTitle: "6.35oz, 1 bar",
-    price: "$5.95",
-    image: "./img/big-bars/milk-chocolate-hazelnut-green.png",
-    alt: "Milk Chocolate Hazelnut Bar"
-  },
-  {
-    title: "Milk Caramel Sea Salt 32%",
-    subTitle: "6.35oz, 1 bar",
-    price: "$5.95",
-    image: "./img/big-bars/milk-chocolate-seasalt-caramel-orange.png",
-    alt: "Milk Caramel Sea Salt Bar"
-  },
-  {
-    title: "Milk Honey Almond Nougat 32%",
-    subTitle: "6.35oz, 1 bar",
-    price: "$5.95",
-    image: "./img/big-bars/chocolate-honey-almond-nougat-yellow.png",
-    alt: "Milk Honey Almond Nougat"
-  },
-  {
-    title: "Dark Almond Sea Salt 51%",
-    subTitle: "6.35oz, 1 bar",
-    price: "$5.95",
-    image: "./img/big-bars/chocolate-almond-seasalt-limegreen.png",
-    alt: "Dark Almond Sea Salt"
-  },
-  {
-    title: "Dark Pecan Coconut 51%",
-    subTitle: "6.35oz, 1 bar",
-    price: "$5.95",
-    image: "./img/big-bars/dark-chocolate-pecan-coconut-teal.png" ,
-    alt: "Dark Pecan Coconut"
-  },
-  {
-    title: "Dark Chocolate 70%",
-    subTitle: "6.35oz, 1 bar",
-    price: "$5.95",
-    image: "./img/big-bars/dark-chocolate-blue.png"  ,
-    alt: "Dark Chocolate"
-  },
-  {
-    title: "Dark Milk Pretzel Toffee 42%",
-    subTitle: "6.35oz, 1 bar",
-    price: "$5.95",
-    image: "./img/big-bars/dark-milk-chocolate-pretzel-toffee-purple.png"  ,
-    alt: "Dark Milk Pretzel Toffee"
-  },
-  {
-    title: "White Raspberry Popping Candy 28%",
-    subTitle: "6.35oz, 1 bar",
-    price: "$5.95",
-    image: "./img/big-bars/white-raspberry-popping-candy-pink.png"  ,
-    alt: "White Raspberry Popping Candy"
-  },
+// const bigBars = [
+//   {
+//     title: "Milk Chocolate 32%",
+//     subTitle: "6.35oz, 1 bar",
+//     price: "$5.95",
+//     image: "./img/big-bars/milk-chocolate-bar-red.png",
+//     alt: "Milk Chocolate Bar"
+//   },
+//   {
+//     title: "Milk Hazelnut 32%",
+//     subTitle: "6.35oz, 1 bar",
+//     price: "$5.95",
+//     image: "./img/big-bars/milk-chocolate-hazelnut-green.png",
+//     alt: "Milk Chocolate Hazelnut Bar"
+//   },
+//   {
+//     title: "Milk Caramel Sea Salt 32%",
+//     subTitle: "6.35oz, 1 bar",
+//     price: "$5.95",
+//     image: "./img/big-bars/milk-chocolate-seasalt-caramel-orange.png",
+//     alt: "Milk Caramel Sea Salt Bar"
+//   },
+//   {
+//     title: "Milk Honey Almond Nougat 32%",
+//     subTitle: "6.35oz, 1 bar",
+//     price: "$5.95",
+//     image: "./img/big-bars/chocolate-honey-almond-nougat-yellow.png",
+//     alt: "Milk Honey Almond Nougat"
+//   },
+//   {
+//     title: "Dark Almond Sea Salt 51%",
+//     subTitle: "6.35oz, 1 bar",
+//     price: "$5.95",
+//     image: "./img/big-bars/chocolate-almond-seasalt-limegreen.png",
+//     alt: "Dark Almond Sea Salt"
+//   },
+//   {
+//     title: "Dark Pecan Coconut 51%",
+//     subTitle: "6.35oz, 1 bar",
+//     price: "$5.95",
+//     image: "./img/big-bars/dark-chocolate-pecan-coconut-teal.png" ,
+//     alt: "Dark Pecan Coconut"
+//   },
+//   {
+//     title: "Dark Chocolate 70%",
+//     subTitle: "6.35oz, 1 bar",
+//     price: "$5.95",
+//     image: "./img/big-bars/dark-chocolate-blue.png"  ,
+//     alt: "Dark Chocolate"
+//   },
+//   {
+//     title: "Dark Milk Pretzel Toffee 42%",
+//     subTitle: "6.35oz, 1 bar",
+//     price: "$5.95",
+//     image: "./img/big-bars/dark-milk-chocolate-pretzel-toffee-purple.png"  ,
+//     alt: "Dark Milk Pretzel Toffee"
+//   },
+//   {
+//     title: "White Raspberry Popping Candy 28%",
+//     subTitle: "6.35oz, 1 bar",
+//     price: "$5.95",
+//     image: "./img/big-bars/white-raspberry-popping-candy-pink.png"  ,
+//     alt: "White Raspberry Popping Candy"
+//   },
        
-];
+// ];
 
-const smallBars = [
-   {
-    title: "Milk Chocolate 32%",
-    subTitle: "1.8oz, 1 bar",
-    price: "$2.39",
-    image: "./img/small-bars/milk-chocolate-small-red.png"  ,
-    alt: "Milk Chocolate"
-  },
-  {
-    title: "Milk Caramel Sea Salt 32%",
-    subTitle: "1.8oz, 1 bar",
-    price: "$2.39",
-    image: "./img/small-bars/milk-caramel-sea-salt-small-orange.png"  ,
-    alt: "Milk Caramel Sea Salt"
-  },
-  {
-    title: "Dark Chocolate 70%",
-    subTitle: "1.8oz, 1 bar",
-    price: "$2.39",
-    image:"./img/small-bars/dark-chocolate-small-blue.png",
-    alt: "Dark Chocolate"
-  },      
-]
+// const smallBars = [
+//    {
+//     title: "Milk Chocolate 32%",
+//     subTitle: "1.8oz, 1 bar",
+//     price: "$2.39",
+//     image: "./img/small-bars/milk-chocolate-small-red.png"  ,
+//     alt: "Milk Chocolate"
+//   },
+//   {
+//     title: "Milk Caramel Sea Salt 32%",
+//     subTitle: "1.8oz, 1 bar",
+//     price: "$2.39",
+//     image: "./img/small-bars/milk-caramel-sea-salt-small-orange.png"  ,
+//     alt: "Milk Caramel Sea Salt"
+//   },
+//   {
+//     title: "Dark Chocolate 70%",
+//     subTitle: "1.8oz, 1 bar",
+//     price: "$2.39",
+//     image:"./img/small-bars/dark-chocolate-small-blue.png",
+//     alt: "Dark Chocolate"
+//   },      
+// ]
 
-const tinyTonys = [
-  {
-    title: "Milk Chocolate 32% Tiny Tony's",
-    subTitle: "100 pieces",
-    price: "$48.69",
-    image:"./img/tiny-tonys/milk-chocolate/milk-chocolate-mini-open-small.png",
-    alt: "Milk Chocolate"
-  },
-  {
-    title: "Milk Caramel Sea Salt 32% Tiny Tony's",
-    subTitle: "100 pieces",
-    price: "$48.69",
-    image:"./img/tiny-tonys/milk-chocolate-caramel-seasalt/milk-chocolate-caramel-open.png" ,
-    alt: "Milk Chocolate Caramel Sea Salt Tiny Tony's"
-  },
-  {
-    title: "Dark Chocolate 70% Tiny Tony's",
-    subTitle: "100 pieces",
-    price: "$48.69",
-    image:"./img/tiny-tonys/dark-chocolate/dark-chocolate-mini-open-large.png"  ,
-    alt: "Dark Chocolate Tiny Tony's"
-  },
-]
+// const tinyTonys = [
+//   {
+//     title: "Milk Chocolate 32% Tiny Tony's",
+//     subTitle: "100 pieces",
+//     price: "$48.69",
+//     image:"./img/tiny-tonys/milk-chocolate/milk-chocolate-mini-open-small.png",
+//     alt: "Milk Chocolate"
+//   },
+//   {
+//     title: "Milk Caramel Sea Salt 32% Tiny Tony's",
+//     subTitle: "100 pieces",
+//     price: "$48.69",
+//     image:"./img/tiny-tonys/milk-chocolate-caramel-seasalt/milk-chocolate-caramel-open.png" ,
+//     alt: "Milk Chocolate Caramel Sea Salt Tiny Tony's"
+//   },
+//   {
+//     title: "Dark Chocolate 70% Tiny Tony's",
+//     subTitle: "100 pieces",
+//     price: "$48.69",
+//     image:"./img/tiny-tonys/dark-chocolate/dark-chocolate-mini-open-large.png"  ,
+//     alt: "Dark Chocolate Tiny Tony's"
+//   },
+// ]
 
 const ChocoShopTotalMenu = bigBars.concat(smallBars).concat(tinyTonys)
 
@@ -125,14 +126,15 @@ export function ChocoShopContainer() {
     <ChocoShop>
 
       <ChocoShop.SidebarContainer>
-        <ChocoShop.SidebarNav>
-        <ul>
-          <a href="/ChocoShop"><li>All Chocolate</li></a>
-          <a href="/ChocoShop"><li>Big Bars</li></a>
-          <a href="/ChocoShop"><li>Small Bars</li></a>
-          <a href="/ChocoShop"><li>Tiny Tony's</li></a>
-        </ul>
-        </ChocoShop.SidebarNav>
+      <ChocoShop.SidebarNav />
+        {/* <ChocoShop.SidebarNav>
+          <ul>
+            <a href="/ChocoShop"><li>All Chocolate</li></a>
+            <a href="/ChocoShop"><li>Big Bars</li></a>
+            <a href="/ChocoShop"><li>Small Bars</li></a>
+            <a href="/ChocoShop"><li>Tiny Tony's</li></a>
+          </ul>
+        </ChocoShop.SidebarNav> */}
       </ChocoShop.SidebarContainer>
 
       <ChocoShop.MainMenuContainer>
@@ -140,9 +142,9 @@ export function ChocoShopContainer() {
           <ul>
             {ChocoShopTotalMenu.map((chocolate) => {
               return (
-                <ChocoShop.MenuItem>
+                <ChocoShop.MenuItem key={chocolate.title + chocolate.price}>
                   <a href="#ChocoShop">
-                    <img src={chocolate.image} alt={chocolate.alt}/>
+                    <img src={chocolate.image} loading="lazy" alt={chocolate.alt}/>
                     <p>{chocolate.title}</p>
                     <p>{chocolate.subTitle}</p>
                     <div>
