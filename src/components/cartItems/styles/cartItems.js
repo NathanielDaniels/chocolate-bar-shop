ced4758aed3204448bd900c13a893ee8750dc4dc
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2023-05-25 16:05:06 -0500

    removed shopping cart total if zero

--- src/components/cartItems/styles/cartItems.js
+++ src/components/cartItems/styles/cartItems.js
@@ -17,16 +17,15 @@ export const Main = styled.main`
   display: flex;
   flex-direction: column;
   width: 550px;
-
   h1 {
     text-align: center;
+    font-size: 3rem;
   }
-
   .total-cost {
     display: inherit;
     justify-content: flex-end;
+    font-size: 1.2rem;
   }
-
   .order-button {
     margin: 0 auto;
     button {
@@ -35,6 +34,16 @@ export const Main = styled.main`
       letter-spacing: 0.4px;
       text-transform: uppercase;
       font-size: 16px;
+      font-weight: 600;
+      background-color: #4caf50;
+      color: #fff;
+      border: none;
+      border-radius: 5px;
+      box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
+      transition: all 0.3s ease-in-out;
+      &:hover {
+        background-color: #3e8e41;
+      }
     }
   }
 `;
@@ -49,19 +58,16 @@ export const CartItem = styled.div`
   display: flex;
   align-items: center;
   margin: 20px 0;
-
   .cart-item {
     display: inherit;
     justify-content: space-between;
     align-items: center;
     width: 100%;
   }
-
   i {
     cursor: pointer;
     margin-right: 20px;
   }
-
   p {
     padding-left: 20px;
   }
