b6eda1281f6f687a67e38f1a4856f3060e803770
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2023-06-07 10:17:13 -0500

    update firebase config

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { GlobalStyles } from './global-styles'
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import App from "./App";
+import { GlobalStyles } from "./global-styles";
 
-import Firebase from 'firebase/app';
+import Firebase from "firebase/app";
 // import 'firebase/firestore';
-import 'firebase/auth';
-import { FirebaseContext } from './context/firebase';
-import { ContextProvider } from './context/Context';
-import Keys from './stash'
+import "firebase/auth";
+import { FirebaseContext } from "./context/firebase";
+import { ContextProvider } from "./context/Context";
+import Keys from "./stash";
 
 const FirebaseConfig = {
   apiKey: Keys().apiKey,
@@ -18,12 +18,12 @@ const FirebaseConfig = {
   storageBucket: "chocolateshop-e8efb.appspot.com",
   messagingSenderId: "292990123053",
   appId: "1:292990123053:web:dda14abf8d57aac0c3cca4",
-  measurementId: "G-LWBVCQEDB2"
+  measurementId: "G-LWBVCQEDB2",
 };
 
 const firebase = Firebase.initializeApp(FirebaseConfig);
 
-ReactDOM.render(
+    render(
   <React.StrictMode>
     <FirebaseContext.Provider value={{ firebase }}>
       <ContextProvider>
@@ -32,6 +32,5 @@ ReactDOM.render(
       </ContextProvider>
     </FirebaseContext.Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
